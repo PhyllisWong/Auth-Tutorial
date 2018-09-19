bbf57250773eecece7bd853e1100b495f33321ef
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-19 16:24:09 -0700

    Add jwt and cookies to sign-up

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,5 +8,22 @@ router.get('/', (req, res) => {
   res.send("Hello Cats!");
 });
 
+//FIXME: Find out if this is the process for Step 7
+// ******  POST: sign-up a User  ********
+router.post('/sign-up', (req, res) => {
+  // Create User and JWT
+  const user = new User(req.body);
+
+  user.save().then( user => {
+    let token = jwt.sign({ _id: user._id}, process.env.SECRET, { expiresIn: "7 days" });
+    res.cookie('nToken', token, {maxAge: 900000, httpOnly: true});
+    res.redirect('/');
+    console.log(req.cookies);
+  })
+
+});
+
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,39 @@
-// Express Module
+// Modules
 const express = require('express');
-const app = express();
+const env = require('dotenv')l
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const authController = require('./controllers/auth.js');
+const app = express();
+
 
 // Middleware
 app.use(methodOverride("_method"));
+// Cookie Parser
+app.use(cookieParser());
 // Body Parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+app.use(jwt({
+  secret: process.env.SECRET,
+  getToken: function fromHeaderOrCookie(req) { //fromHeaderOrQueryString
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.cookies && req.cookies.token) {
+      return req.cookies.token;
+    }
+    return null;
+  }
+}).unless({path: ['/', '/login', 'sign-up']}));
 
 // Routes
 app.use("", authController);
 
-
+//FIXME: Complete Step 2
 
 // Server
 app.listen(3000, () => {
