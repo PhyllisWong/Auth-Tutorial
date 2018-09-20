cd08e06700b47bfd4bbf903b1ae9f1fc3c966fc0
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-20 11:27:29 -0700

    Add handlebars to use forms

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Modules
 const express = require('express');
-const env = require('dotenv')l
+const hbs = require('express-handlebars');
+const env = require('dotenv');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -9,17 +10,28 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const authController = require('./controllers/auth.js');
 const app = express();
+const port = process.env.PORT || 3000;
 
 
 // Middleware
 app.use(methodOverride("_method"));
 // Cookie Parser
 app.use(cookieParser());
+// Handlebars
+app.engine('hbs', hbs({defaultLayout: 'main', extname: 'hbs'}));
+app.set('view engine', 'hbs');
+
+
+// static content
+app.use(express.static('./public'));
+
 // Body Parser
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(jwt({
   secret: process.env.SECRET,
+  audience: 'http://localhost:3000',
+  issuer: 'http://localhost:3000',
   getToken: function fromHeaderOrCookie(req) { //fromHeaderOrQueryString
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
       return req.headers.authorization.split(' ')[1];
@@ -35,7 +47,14 @@ app.use("", authController);
 
 //FIXME: Complete Step 2
 
+/ Mongoose Connection
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/jwt-token";
+mongoose.connect(
+  mongoUri, { useNewUrlParser: true }
+);
+mongoose.set('debug', true);
+
 // Server
-app.listen(3000, () => {
-  console.log("Server listening on 3000!");
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
 });
