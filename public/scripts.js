2bee2310d348e2e3c6630a6de5aad8c7e8b7e03d
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-21 09:08:12 -0700

    Wrap post request in an object

--- public/scripts.js
+++ public/scripts.js
@@ -1,24 +1,23 @@
 
-  $( document ).ready(() => {
-    $("#signup").submit(function(e) {
-      var form = $(this);
-      var url = form.attr('action');
-      $.post({
-        url: url,
-        data: form.serialize(),
-        success: ( res ) => {
-          Cookies.set('token', res);
-          window.location.href = "/success";
-        }
-      })
-      e.preventDefault();
+$( document ).ready(() => {
+  $("#signup").submit(function(e) {
+    let form = $(this);
+    let url = form.attr('action');
+    $.post({
+      url: url,
+      data: form.serialize(),
+      success: ( res ) => {
+        Cookies.set('token', res);
+        window.location.href = "/success";
+      }
     });
-
-  $('#sign-up-btn').on('click', function(e) {
-    e.preventDefault(); // <= force the form to not redirect
-
-    location.href = "/sucess";
+    e.preventDefault();
   });
 
+$('#sign-up-btn').on('click', function(e) {
+  e.preventDefault(); // <= force the form to not redirect
+
+  location.href = "/sucess";
+})
 
 
